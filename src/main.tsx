3687ad32e324139b3dd2c836184f5fb81cfa2cca
Mina <minayeh1347@gmail.com>
2026-02-12 04:28:06 -0500

    center

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import "@fontsource/playfair-display/index.css";
+import "@fontsource/playfair-display/400-italic.css";
+
 
 
 createRoot(document.getElementById('root')!).render(
