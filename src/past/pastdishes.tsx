085c8f2b2d9b10711c35d7c09058bdd6d136e3a6
Mina <minayeh1347@gmail.com>
2026-02-25 05:15:07 -0500

    remove babel

---
+++ src/past/pastdishes.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PastDishes() {
+  return (
+    <div style={{ padding: "100px", backgroundColor: "#f8f8f8" }}>
+      <h1>Past Dishes</h1>
+      <p>Here are all previous dishes.</p>
+    </div>
+  );
+}
